df112575f5c97c8fdd36a9b4dfd1f9a2cd52d781
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-04-04 02:10:00 +0600

    in home page product info + customer review section

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,59 @@
 import React from 'react';
+import i1 from '../../Assets/Images/1.png';
 
 const Home = () => {
     return (
-        <div>
-            <h2>Home</h2>
+        <div className=''>
+            <section className="text-gray-600 body-font">
+                <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+                    <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-bold text-gray-800">Best TV of 2022</h1>
+                        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-bold text-indigo-500">Best TV of 2022</h1>
+                        <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+                        <div className="flex justify-center">
+                            <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg font-bold">Buy Now</button>
+                        </div>
+                    </div>
+                    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+                        <img className="object-cover object-center rounded" alt="hero" src={i1} />
+                    </div>
+                </div>
+            </section>
+
+            <section className="text-gray-600 body-font">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="flex flex-wrap -m-4">
+                        <div className="p-4 md:w-1/3">
+                            <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                                <img className="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/720x400" alt="blog" />
+                                <div className="p-6">
+                                    <h1 className="title-font text-lg font-medium text-gray-900 mb-3">The Catalyzer</h1>
+                                    <p className="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="p-4 md:w-1/3">
+                            <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                                <img className="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/721x401" alt="blog" />
+                                <div className="p-6">
+                                    <h1 className="title-font text-lg font-medium text-gray-900 mb-3">The 400 Blows</h1>
+                                    <p className="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="p-4 md:w-1/3">
+                            <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                                <img className="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/722x402" alt="blog" />
+                                <div className="p-6">
+                                    <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Shooting Stars</h1>
+                                    <p className="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg font-bold">See All Reviews</button>
+            </section>
         </div>
     );
 };
