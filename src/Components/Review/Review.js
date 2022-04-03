04cc0e814f91fe8f5cb7c79518f84c748997da35
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-04-04 02:24:13 +0600

    header & review components created

---
+++ src/Components/Review/Review.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Review = () => {
+    return (
+        <div className="p-4 md:w-1/3">
+            <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                <img className="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/720x400" alt="blog" />
+                <div className="p-6">
+                    <h1 className="title-font text-lg font-medium text-gray-900 mb-3">The Catalyzer</h1>
+                    <p className="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Review;
--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Review from '../Review/Review';
 
 const Reviews = () => {
     return (
-        <div>
-            <h2>Reviews</h2>
-        </div>
+        <section className="text-gray-600 body-font">
+            <div className="container px-5 py-24 mx-auto">
+                <div className="flex flex-wrap -m-4">
+                    {
+                        <Review />
+                    }
+                </div>
+            </div>
+            <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg font-bold">See All Reviews</button>
+        </section>
     );
 };
 
