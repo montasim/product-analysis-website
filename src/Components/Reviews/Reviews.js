e01135c6a36e50602928e4543ef1b82d70dca8a1
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-04-04 03:35:29 +0600

    created & used react custom hook for reviews data

--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -1,14 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import LoadReviews from '../../Hooks/LoadReviews';
 import Review from '../Review/Review';
 
 const Reviews = () => {
-    const [reviews, setReviews] = useState([]);
-
-    useEffect(() => {
-        fetch('data.json')
-            .then(res => res.json())
-            .then(data => setReviews(data));
-    }, []);
+    const [reviews, setReviews] = LoadReviews();
 
     return (
         <section className="text-gray-600 body-font">
