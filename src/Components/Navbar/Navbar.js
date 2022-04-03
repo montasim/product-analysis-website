3b918caa67e721e29f9fbb2e74aca32ebee0b67a
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-04-03 23:22:49 +0600

    onclick navbar link prevent reloading

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className='mx-4 py-8 flex justify-center gap-4 uppercase font-bold'>
-            <a href="/home">Home</a>
-            <a href="/reviews">Reviews</a>
-            <a href="/dashboard">Dashboard</a>
-            <a href="/blogs">Blogs</a>
-            <a href="/about">About</a>
+            <Link to="/home">Home</Link>
+            <Link to="/reviews">Reviews</Link>
+            <Link to="/dashboard">Dashboard</Link>
+            <Link to="/blogs">Blogs</Link>
+            <Link to="/about">About</Link>
         </nav>
     );
 };
