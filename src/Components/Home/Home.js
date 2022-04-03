feaa99ac9f1987873f857a093eca38e8bc9c55bf
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-04-04 03:20:00 +0600

    customer review text in home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -10,6 +10,9 @@ const Home = () => {
         <div className=''>
             <Header />
 
+            <h1 className="title-font sm:text-3xl text-center text-3xl mb-4 font-bold text-grey-800">Customer Reviews (3)</h1>
+            <hr />
+
             <Reviews />
 
             <button onClick={() => navigate("/reviews")} className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg font-bold">See All Reviews</button >
--- src/Components/Review/Review.js
+++ src/Components/Review/Review.js
@@ -7,12 +7,12 @@ const Review = (props) => {
 
     return (
         <div className="p-4 md:w-1/3">
-            <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+            <div className="h-full border-2 rounded-lg items-center">
                 <img className="lg:h-48 md:h-36 w-50 object-cover object-center" src={image} alt="blog" />
                 <div className="p-6">
                     <h1 className="title-font text-lg font-medium text-gray-900 mb-3">{name}</h1>
                     <p className="leading-relaxed mb-3">{review_text}</p>
-                    <p className='title-font'>Rating <span className='text-xl'>{rating}</span></p>
+                    <p className='title-font'>Rating <span className='text-xl font-bold'>{rating}</span></p>
                 </div>
             </div>
         </div>
