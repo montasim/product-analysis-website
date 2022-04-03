9804e0348d3cf20b724dc72b60f1e9f4807186de
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-04-03 23:33:00 +0600

    active link is now orange color

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
 
 const Navbar = () => {
     return (
-        <nav className='mx-4 py-8 flex justify-center gap-4 uppercase font-bold'>
-            <Link to="/home">Home</Link>
-            <Link to="/reviews">Reviews</Link>
-            <Link to="/dashboard">Dashboard</Link>
-            <Link to="/blogs">Blogs</Link>
-            <Link to="/about">About</Link>
+        <nav className='mx-4 py-8 flex justify-center gap-6 uppercase font-bold'>
+            <CustomLink to="/home">Home</CustomLink>
+            <CustomLink to="/reviews">Reviews</CustomLink>
+            <CustomLink to="/dashboard">Dashboard</CustomLink>
+            <CustomLink to="/blogs">Blogs</CustomLink>
+            <CustomLink to="/about">About</CustomLink>
         </nav>
     );
 };
