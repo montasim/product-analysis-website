24f95447d620b35245ee4023e1fa451e63678e2b
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-04-04 15:37:41 +0600

    navbar is mobile responsive

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import CustomLink from '../CustomLink/CustomLink';
 
 const Navbar = () => {
     return (
-        <nav className='mx-4 mt-8 py-8 flex justify-center gap-6 text-xl uppercase font-bold'>
-            <CustomLink to="/home">Home</CustomLink>
-            <CustomLink to="/reviews">Reviews</CustomLink>
-            <CustomLink to="/dashboard">Dashboard</CustomLink>
-            <CustomLink to="/blogs">Blogs</CustomLink>
-            <CustomLink to="/about">About</CustomLink>
-        </nav>
+        <header class="text-gray-600 body-font">
+            <div class="container mx-auto flex flex-wrap mt-12 p-5 flex-col md:flex-row items-center">
+                <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center gap-6 uppercase font-bold">
+                    <CustomLink to="/home">Home</CustomLink>
+                    <CustomLink to="/reviews">Reviews</CustomLink>
+                    <CustomLink to="/dashboard">Dashboard</CustomLink>
+                    <CustomLink to="/blogs">Blogs</CustomLink>
+                    <CustomLink to="/about">About</CustomLink>
+                </nav>
+            </div>
+        </header>
     );
 };
 
