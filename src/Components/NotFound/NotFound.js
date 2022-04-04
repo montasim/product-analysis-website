4bf46122c58a716853382978dc9fb3e0fd060b8c
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-04-04 17:15:44 +0600

    404 error page added

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import e404 from '../../Assets/Images/404.png';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>Not Found</h2>
-        </div>
+        <section class="mx-12 text-gray-600 body-font">
+            <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+                <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src={e404} />
+            </div>
+        </section>
     );
 };
 
